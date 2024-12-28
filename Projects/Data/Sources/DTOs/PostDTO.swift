943f5c163af5859c8f3c159d7bba0ec80687e779
hankyeol-dev <ddovlek@kakao.com>
2024-12-28 16:32:36 +0900

    [API/Router] Post Router 추가 작성

--- Projects/Data/Sources/DTOs/PostDTO.swift
+++ Projects/Data/Sources/DTOs/PostDTO.swift
@@ -2,6 +2,58 @@
 
 import Foundation
 
+public struct PostInputType: Encodable {
+   let category: String
+   let title: String
+   let price: Int
+   let content: String
+   let content1: String?
+   let content2: String?
+   let content3: String?
+   let content4: String?
+   let content5: String?
+   let files: [String]?
+   let logitude: Float?
+   let latitude: Float?
+   
+   public init(
+      category: String,
+      title: String,
+      price: Int,
+      content: String, 
+      content1: String?,
+      content2: String?,
+      content3: String?, 
+      content4: String?,
+      content5: String?,
+      files: [String]?,
+      logitude: Float?,
+      latitude: Float?) {
+      self.category = category
+      self.title = title
+      self.price = price
+      self.content = content
+      self.content1 = content1
+      self.content2 = content2
+      self.content3 = content3
+      self.content4 = content4
+      self.content5 = content5
+      self.files = files
+      self.logitude = logitude
+      self.latitude = latitude
+   }
+}
+
 public struct UploadFileInputType: Encodable {
    let files: [Data]
+   
+   public init(files: [Data]) {
+      self.files = files
+   }
+}
+
+public struct GetPostQueryType {
+   public let next: String
+   public let limit: Int = 5
+   public let category: String
 }
--- Projects/Data/Sources/Network/Routers/PostRouter.swift
+++ Projects/Data/Sources/Network/Routers/PostRouter.swift
@@ -4,29 +4,48 @@ import Foundation
 import Moya
 
 public enum PostRouter {
+   case uploadPost(input: PostInputType)
    case uploadFiles(input: UploadFileInputType)
+   case getPost(postId: String)
+   case getPosts(query: GetPostQueryType)
 }
 
 extension PostRouter: RouterType {
    public var path: String {
       switch self {
+      case .uploadPost: return "/posts"
       case .uploadFiles: return "/posts/files"
+      case let .getPost(postId): return "/posts/\(postId)"
+      case .getPosts: return "/posts"
       }
    }
    
    public var method: Moya.Method {
       switch self {
-      case .uploadFiles: return .post
+      case .getPosts, .getPost: return .get
+      case .uploadPost, .uploadFiles: return .post
       }
    }
    
    public var task: Moya.Task {
       switch self {
+      case let .uploadPost(input):
+         let body = taskMapper(input)
+         return .requestParameters(parameters: body, encoding: JSONEncoding.default)
       case let .uploadFiles(input):
          let files = input.files.map({
             return MultipartFormData(provider: .data($0), name: "files", fileName: "file")
          })
          return .uploadMultipart(files)
+      case let .getPosts(query):
+         let queries: [String: String] = [
+            "next": query.next.isEmpty ? "" : query.next,
+            "limit": String(query.limit),
+            "category": query.category
+         ]
+         return .requestParameters(parameters: queries, encoding: URLEncoding.queryString)
+      default:
+         return .requestPlain
       }
    }
    
@@ -37,6 +56,8 @@ extension PostRouter: RouterType {
       switch self {
       case .uploadFiles:
          return generateHeaderFields(true, .multipart, productIdFiels)
+      default:
+         return generateHeaderFields(true, .base, productIdFiels)
       }
    }
 }
