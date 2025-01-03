6a15297187264cfd2f3929148754370a43146e76
hankyeol-dev <ddovlek@kakao.com>
2025-01-03 13:46:40 +0900

    [UserInfo] Domain에서 UserInfo, KingfisherManager 컨트롤

---
+++ Projects/Domain/Sources/Utils/UserInfo/UserDefaultsWrapper.swift
@@ -0,0 +1,34 @@
+// hankyeol-dev. Domain
+
+import Foundation
+
+@frozen
+public enum UserDefaultsKeys: String {
+   case userId
+   case productId
+   case accessToken
+   case refreshToken
+   case isLogined
+}
+
+@propertyWrapper
+public struct UserDefaultsWrapper<T> {
+   private let standard = UserDefaults.standard
+   private let key: UserDefaultsKeys
+   let defaultValue: T
+   
+   public init(key: UserDefaultsKeys, defaultValue: T) {
+      self.key = key
+      self.defaultValue = defaultValue
+   }
+   
+   public var wrappedValue: T {
+      get {
+         standard.value(forKey: key.rawValue) as? T ?? defaultValue
+      }
+      
+      set {
+         standard.setValue(newValue, forKey: key.rawValue)
+      }
+   }
+}
