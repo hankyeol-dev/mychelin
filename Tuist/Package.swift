86d66a48416ffd9e068dae6452e6203422b4385e
hankyeol-dev <ddovlek@kakao.com>
2025-01-04 09:58:13 +0900

    [Tuist] RxDataSource 패키지 반영 및 기존 외부 프레임워크 활용 방식 변경 - as-is: staticLibrary - to-be: dynamicFramework - 변경 사유: failed to demangle superclass .. 관련 에러 (참고 - https://minryul.tistory.com/28)

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -7,17 +7,19 @@ import ProjectDescriptionHelpers
 
 let packageSettings = PackageSettings(
    productTypes: [
-      "Moya": .staticLibrary,
-      "RxMoya": .staticLibrary,
-      "ReactiveMoya": .staticLibrary,
-      "RxSwift": .staticLibrary,
-      "RxCocoa": .staticLibrary,
-      "RxCocoaRuntime": .staticLibrary,
-      "ReactorKit": .staticLibrary,
-      "Kingfisher": .staticLibrary,
-      "Then": .staticLibrary,
-      "SnapKit": .staticLibrary,
-      "SkyFloatingLabelTextField": .staticLibrary
+      "Moya": .framework,
+      "RxMoya": .framework,
+      "ReactiveMoya": .framework,
+      "RxSwift": .framework,
+      "RxCocoa": .framework,
+      "RxCocoaRuntime": .framework,
+      "RxDataSources": .framework,
+      "ReactorKit": .framework,
+      "Kingfisher": .framework,
+      "Then": .framework,
+      "SnapKit": .framework,
+      "SkyFloatingLabelTextField": .framework,
+      "Reusable": .framework
    ],
    baseSettings: .settings(configurations: [
       .debug(name: ProjectConfiguration.debugConfig)
@@ -35,10 +37,12 @@ let package = Package(
       .package(url: "https://github.com/Moya/Moya.git", from: "15.0.0"),
       .package(url: "https://github.com/ReactiveX/RxSwift.git", branch: "main"),
       .package(url: "https://github.com/ReactorKit/ReactorKit.git", from: "3.2.0"),
+      .package(url: "git@github.com:RxSwiftCommunity/RxDataSources.git", branch: "main"),
       .package(url: "git@github.com:onevcat/Kingfisher.git", branch: "master"),
       .package(url: "https://github.com/devxoul/Then.git", from: "3.0.0"),
       .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.7.1"),
-      .package(url: "https://github.com/Skyscanner/SkyFloatingLabelTextField.git", from: "3.8.0")
+      .package(url: "https://github.com/Skyscanner/SkyFloatingLabelTextField.git", from: "3.8.0"),
+      .package(url: "https://github.com/AliSoftware/Reusable.git", branch: "main")
    ],
    targets: [
       .target(name: "Slp2_Project", dependencies: ["RxMoya", "ReactiveMoya"])
