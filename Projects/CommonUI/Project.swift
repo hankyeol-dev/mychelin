f8aaae8e2439034961524ae8b9a30b2f4528e00c
hankyeol-dev <ddovlek@kakao.com>
2025-01-04 09:54:32 +0900

    [CommonUI] CommonUI 모듈에서도 Asset을 가질 수 있도록 프로젝트 세팅

--- Projects/CommonUI/Project.swift
+++ Projects/CommonUI/Project.swift
@@ -11,6 +11,7 @@ import ProjectDescriptionHelpers
 let project = Project.make(
    module: .commonUI,
    product: .framework,
+   needResources: true,
    targetDependencies: [
       .project(target: Module.domain.toName, path: Module.domain.toPath),
       .external(name: Library.snapkit.rawValue),
@@ -18,6 +19,8 @@ let project = Project.make(
       .external(name: Library.rxSwift.rawValue),
       .external(name: Library.textField.rawValue),
       .external(name: Library.then.rawValue),
-      .external(name: Library.kingfisher.rawValue)
+      .external(name: Library.kingfisher.rawValue),
+      .external(name: Library.rxData.rawValue),
+      .external(name: Library.reusable.rawValue)
    ]
 )
