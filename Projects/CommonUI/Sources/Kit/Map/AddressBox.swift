d6599d083061d3f3e4799470c38b71daa1e110a7
hankyeol-dev <ddovlek@kakao.com>
2025-01-07 17:30:35 +0900

    [MapView] MapView 구성을 위한 CommonUI 구축

---
+++ Projects/CommonUI/Sources/Kit/Map/AddressBox.swift
@@ -0,0 +1,46 @@
+// hankyeol-dev. CommonUI
+
+import UIKit
+import SnapKit
+import Then
+
+public final class AddressBox: BaseView {
+   private let back: UIView = .init().then {
+      $0.backgroundColor = .white
+      $0.layer.cornerRadius = 10.0
+   }
+   private let icon: UIImageView = .init().then {
+      $0.image = .pin.withRenderingMode(.alwaysTemplate)
+      $0.contentMode = .scaleAspectFit
+      $0.tintColor = .grayMd
+   }
+   private let address: BaseLabel = .init(.init(style: .base))
+   
+   override func setSubviews() {
+      super.setSubviews()
+      addSubview(back)
+      back.addSubviews(icon, address)
+   }
+   
+   override func setLayouts() {
+      super.setLayouts()
+      let safe: CGFloat = 20.0
+      back.snp.makeConstraints { make in
+         make.height.equalTo(safe * 2)
+         make.horizontalEdges.equalToSuperview()
+      }
+      icon.snp.makeConstraints { make in
+         make.centerY.equalToSuperview()
+         make.size.equalTo(safe)
+         make.leading.equalTo(safeAreaLayoutGuide).inset(safe - 5.0)
+      }
+      address.snp.makeConstraints { make in
+         make.centerY.equalTo(icon.snp.centerY)
+         make.leading.equalTo(icon.snp.trailing).offset(safe - 5.0)
+      }
+   }
+   
+   public func setAddress(_ addressString: String) {
+      address.setText(addressString)
+   }
+}
---
+++ Projects/CommonUI/Sources/Kit/Map/MapAnnotationView.swift
@@ -0,0 +1,46 @@
+// hankyeol-dev. CommonUI
+
+import UIKit
+import MapKit
+import Then
+import SnapKit
+
+public final class MapAnnotationView: MKAnnotationView {
+   private let pinView: UIImageView = .init().then {
+      $0.image = .pin
+      $0.image?.withRenderingMode(.alwaysTemplate)
+   }
+   
+   public override init(annotation: MKAnnotation?, reuseIdentifier: String?) {
+      super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+      addSubview(pinView)
+      pinView.snp.makeConstraints { make in
+         make.size.equalTo(50.0)
+      }
+   }
+   
+   public override func prepareForReuse() {
+      super.prepareForReuse()
+      pinView.image = nil
+   }
+   
+   public override func prepareForDisplay() {
+      super.prepareForDisplay()
+      guard let annotation = annotation as? AnnotationType else { return }
+      
+      pinView.tintColor = annotation.pinColor
+      
+      setNeedsLayout()
+   }
+   
+   public override func layoutSubviews() {
+      super.layoutSubviews()
+      bounds.size = CGSize(width: 50.0, height: 50.0)
+      centerOffset = CGPoint(x: 0.0, y: -25.0)
+   }
+   
+   @available(*, unavailable)
+   required init?(coder aDecoder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+   }
+}
