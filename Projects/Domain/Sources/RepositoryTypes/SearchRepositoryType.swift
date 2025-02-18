02842c791d5352c6d644d2345c9d17f3bf095750
hankyeol-dev <ddovlek@kakao.com>
2025-02-18 23:12:05 +0900

    [API] Mocking File 분리

--- Projects/Domain/Sources/RepositoryTypes/SearchRepositoryType.swift
+++ Projects/Domain/Sources/RepositoryTypes/SearchRepositoryType.swift
@@ -6,7 +6,3 @@ import RxSwift
 public protocol SearchRepositoryType {
    func nLocationSearch(query: String, start: Int) -> Single<Result<[NaverSearchVO], NetworkErrors>>
 }
-
-public protocol MockSearchRepositoryType {
-   func kLocationSearch(query: String) async -> Result<KSearchVO, CommonError>
-}
--- Projects/Domain/Sources/RepositoryTypes/UserRepositoryType.swift
+++ Projects/Domain/Sources/RepositoryTypes/UserRepositoryType.swift
@@ -6,7 +6,3 @@ import RxSwift
 public protocol UserRepositoryType {
    func getMe() -> Single<Result<MeProfileVO, NetworkErrors>>
 }
-
-public protocol MockUserRepositoryType {
-   func getMe() async -> Result<MeProfileVO, CommonError>
-}
